3805a5e2a3527c2bfa3717686103b27a61629c00
Vishal <vismdbs@outlook.com>
2023-02-19 10:01:28 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,46 @@ import ProfileCard from "./ProfileCard";
 import AlexImage from "./img/alexa.png";
 import SiriImage from "./img/siri.png";
 import CortanaImage from "./img/cortana.png";
+import "bulma/css/bulma.css";
 
 export default function App() {
   return (
     <div>
-      <ProfileCard aiName="Alexa" twitterHandle="@alexa99" image={AlexImage} />
-      <ProfileCard aiName="Siri" twitterHandle="@siri01" image={SiriImage} />
-      <ProfileCard
-        aiName="Cortana"
-        twitterHandle="@cortana32"
-        image={CortanaImage}
-      />
+      <section className="hero is-primary">
+        <p className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </p>
+      </section>
+      <div class="container">
+        <section class="section">
+          <div class="columns">
+            <div className="column is-4">
+              <ProfileCard
+                aiName="Alexa"
+                twitterHandle="@alexa99"
+                image={AlexImage}
+                desc="Created by Amazon helps you buy things and stuff"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                aiName="Siri"
+                twitterHandle="@siri01"
+                image={SiriImage}
+                desc="Created by Apple. Complete BS"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                aiName="Cortana"
+                twitterHandle="@cortana32"
+                image={CortanaImage}
+                desc="Created by Microsoft. Complete BS"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,8 +1,18 @@
 export default function ProfileCard(propObj) {
   return (
-    <div>
-      <img src={propObj.image} alt="Logo" />
-      {propObj.aiName} {propObj.twitterHandle}
+    <div className="card">
+      <div class="card-image">
+        <figure class="image is1by1">
+          <img src={propObj.image} alt="Logo" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media-content">
+          <p class="title is-4">{propObj.aiName}</p>{" "}
+          <p class="subtitle is-6">{propObj.twitterHandle}</p>
+        </div>
+        <div className="content">{propObj.desc}</div>
+      </div>
     </div>
   );
 }
